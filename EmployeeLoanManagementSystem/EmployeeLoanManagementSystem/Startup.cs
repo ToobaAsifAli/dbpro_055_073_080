37fb9b6cd6d49f506468127c4edde1d934a86592
ToobaAsifAli <34550387+ToobaAsifAli@users.noreply.github.com>
2019-04-14 04:21:18 +0500

    front end

---
+++ EmployeeLoanManagementSystem/EmployeeLoanManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeLoanManagementSystem.Startup))]
+namespace EmployeeLoanManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
